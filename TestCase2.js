5ebd9cb1a645f448e503506fa429dcfd41ccfc36
Sherina Eria <hastalinas@gmail.com>
2024-12-24 20:58:04 +0700

    TestCase2 - Login Invalid

--- TestCase2.js
+++ TestCase2.js
@@ -1,6 +1,13 @@
 const { Builder } = require('selenium-webdriver');
 const LoginPage = require('./WebComponent/LoginPage');
 const assert = require('assert');
+require('dotenv').config();
+const fs = require('fs');
+
+const screenshotDir = './screenshot/';
+if(!fs.existsSync(screenshotDir)){
+    fs.mkdirSync(screenshotDir, {recursive: true});
+}
 
 describe('TestCase 2', function(){
     this.timeout(40000);
@@ -23,6 +30,13 @@ describe('TestCase 2', function(){
         const loginPage = new LoginPage(driver);
         const errorMessage = await loginPage.getErrorMessage();
         assert.strictEqual(errorMessage, 'Epic sadface: Username and password do not match any user in this service', 'Expected error message does not match');
+    });
+
+    // take screenshot
+    afterEach(async function () {
+        const screenshot = await driver.takeScreenshot();
+        const filepath = `${screenshotDir}${this.currentTest.title.replace(/\s+/g, '_')}_${Date.now()}.png`
+        fs.writeFileSync(filepath, screenshot, 'base64');
     });
 
     after(async function () {
